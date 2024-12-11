4fa9244340ba5343e32305d948901f6a3b473092
Pavel Loubek <pav.loubek@gmail.com>
2024-12-11 17:19:00 +0100

    AoC2 part 2 close, but too high!

--- AoC2.py
+++ AoC2.py
@@ -4,13 +4,13 @@ print("Please insert your full puzzle input:")
 # Vars
 list = []
 resultPart1 = 0
+resultPart2 = 0
 listAscending = True
 minVar = 1
 maxVar = 3
 emptyInput = False
 
-# Part 1
-# Add while to add more inputs
+# Main - the 2 parts could be together and work simultaneously, but I like them separated here.
 while(emptyInput == False):
     inputLine = input()
     if inputLine != "":
@@ -22,6 +22,8 @@ while(emptyInput == False):
             listAscending = False
         else:
             continue
+
+        # Part 1
         for i in range(len(list)-1):
             if listAscending:
                 if list[i] < list[i+1] and list[i]+maxVar >= list[i+1]:
@@ -37,9 +39,50 @@ while(emptyInput == False):
                     continue
                 else:
                     break
-            # Check first two numbers if descending or ascending
-            # Then check each
+        
+        # Part 2
+        problemDampener = False
+        for i in range(len(list)-1):
+            if listAscending:
+                if list[i] < list[i+1] and list[i]+maxVar >= list[i+1]:
+                    if i+1 == len(list)-1:
+                        resultPart2+=1
+                    continue
+                elif problemDampener == False:
+                    try: 
+                        list[i+2]
+                    except:
+                        resultPart2+=1
+                        break
+                    else:
+                        if list[i] < list[i+2] and list[i]+maxVar >= list[i+2]:
+                            i+=1
+                            problemDampener == True
+                            continue
+                else:
+                    break
+            else:
+                if list[i] > list[i+1] and list[i]-maxVar <= list[i+1]:
+                    if i+1 == len(list)-1:
+                        resultPart2+=1
+                    continue
+                elif problemDampener == False:
+                    try: 
+                        list[i+2]
+                    except:
+                        resultPart2+=1
+                        break
+                    else:
+                        if list[i] > list[i+2] and list[i]-maxVar <= list[i+2]:
+                            i+=1
+                            problemDampener == True
+                            continue
+                else:
+                    break
+
+    # Finish
     else:
         print("Result for part 1 is " + str(resultPart1))
+        print("Result for part 2 is " + str(resultPart2))
         emptyInput == True
         break
