a03c22981c493ca6a403393cab74a666e0912534
Pavel Loubek <pav.loubek@gmail.com>
2024-12-11 14:55:15 +0100

    Added (not working) base of part 2

--- AoC1.py
+++ AoC1.py
@@ -1,9 +1,14 @@
+# Info
 print("Please insert your full puzzle input:")
 
+# Vars
 emptyInput = False
 list1 = []
 list2 = []
-listResults = []
+listResultsPart1 = []
+listResultsPart2 = []
+
+# List input
 while(emptyInput == False):
     inputLine = input()
     if inputLine == "":
@@ -14,15 +19,27 @@ while(emptyInput == False):
     list2.append(inputsLine[1])
 list1.sort()
 list2.sort()
+
+# Part1
 listRange = range(len(list1))
 for x in listRange:
     num1 = int(list1[x])
     num2 = int(list2[x])
     if num1 > num2:
         res = num1 - num2
-        listResults.append(res)
+        listResultsPart1.append(res)
     else:
         res = num2 - num1
-        listResults.append(res)
-result = sum(listResults)
-print(result)
+        listResultsPart1.append(res)
+result = sum(listResultsPart1)
+print("Result for part 1 is " + str(result))
+
+# Part2
+listRange = range(len(list1))
+for x in listRange:
+    countNum1 = list1.count(list1[x])
+    countNum2 = list2.count(list1[x])
+    res = countNum1 * countNum2
+    listResultsPart2.append(res)
+result = sum(listResultsPart2)
+print("Result for part 2 is " + str(result))
