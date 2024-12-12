3963cf2759b903359e3bc56332b6b1fdaea5e17a
Pavel Loubek <pav.loubek@gmail.com>
2024-12-12 17:31:11 +0100

    AoC2 Finished - painful code, but lazy to fix

--- AoC2.py
+++ AoC2.py
@@ -6,6 +6,7 @@ list = []
 resultPart1 = 0
 resultPart2 = 0
 listAscending = True
+listEquals = False
 minVar = 1
 maxVar = 3
 emptyInput = False
@@ -21,11 +22,13 @@ while(emptyInput == False):
         elif list[0] > list[1]:
             listAscending = False
         else:
-            continue
+            listEquals = True
 
         # Part 1
         for i in range(len(list)-1):
-            if listAscending:
+            if listEquals:
+                break
+            elif listAscending:
                 if list[i] < list[i+1] and list[i]+maxVar >= list[i+1]:
                     if i+1 == len(list)-1:
                         resultPart1+=1
@@ -40,55 +43,82 @@ while(emptyInput == False):
                 else:
                     break
         
-        # Part 2
+        # Part 2 - a very bad for range happened, not used to Python and how it works, but keeping the workaround
         problemDampener = False
-        listEquals = False
+        ascendingCount = 0
+        descendingCount = 0
 
-        if list[0] == list[1]:
+        if listEquals:
             list.remove(list[0])
             problemDampener = True
+            listEquals = False
+        if list[0]+maxVar <= list[1] or list[0]-maxVar >= list[1]:
+            list.remove(list[0])
+            problemDampener = True
+        # This one is painful but I'm too deep in my thoughts to care
+        for j in range(len(list)-1):
+            if list[j] < list[j+1]:
+                ascendingCount+=1
+            elif list[j] > list[j+1]:
+                descendingCount+=1
+            else:
+                continue
+        if ascendingCount > descendingCount and descendingCount < 2:
+            listAscending = True
+        elif descendingCount > ascendingCount and ascendingCount < 2:
+            listAscending = False
+        else:
+            continue
         for i in range(len(list)-1):
-            print (i,list[i],list[i+1])
+            index = i
+            if problemDampener:
+                index+=1
+                try: 
+                    list[index+1]
+                except:
+                    resultPart2+=1
+                    break
+            # print (index,list[index],list[index+1])
             if listAscending:
-                if list[i] < list[i+1] and list[i]+maxVar >= list[i+1]:
-                    if i+1 == len(list)-1:
+                if list[index] < list[index+1] and list[index]+maxVar >= list[index+1]:
+                    if index+1 >= len(list)-1:
                         resultPart2+=1
+                        break
                     continue
                 elif problemDampener == False:
                     try: 
-                        list[i+2]
+                        list[index+2]
                     except:
                         resultPart2+=1
                         break
                     else:
-                        if list[i] < list[i+2] and list[i]+maxVar >= list[i+2]:
-                            i+=1
-                            problemDampener == True
+                        if list[index] < list[index+2] and list[index]+maxVar >= list[index+2]:
+                            # index+=1
+                            problemDampener = True
                             continue
                         else:
-                            i+=1
-                            continue
+                            break
                 else:
                     break
             else:
-                if list[i] > list[i+1] and list[i]-maxVar <= list[i+1]:
-                    if i+1 == len(list)-1:
+                if list[index] > list[index+1] and list[index]-maxVar <= list[index+1]:
+                    if index+1 >= len(list)-1:
                         resultPart2+=1
+                        break
                     continue
                 elif problemDampener == False:
                     try: 
-                        list[i+2]
+                        list[index+2]
                     except:
                         resultPart2+=1
                         break
                     else:
-                        if list[i] > list[i+2] and list[i]-maxVar <= list[i+2]:
-                            i+=1
-                            problemDampener == True
+                        if list[index] > list[index+2] and list[index]-maxVar <= list[index+2]:
+                            # index+=1
+                            problemDampener = True
                             continue
                         else:
-                            i+=1
-                            continue
+                            break
                 else:
                     break
 
