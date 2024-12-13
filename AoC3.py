92e15c37c0439c6d407c8c8411d66b12b643c498
Pavel Loubek <pav.loubek@gmail.com>
2024-12-13 10:11:30 +0100

    AoC3 regex base

--- AoC3.py
+++ AoC3.py
@@ -1,10 +1,15 @@
+# Modules - oh no, regex time :<
+import re
+
 # Info
 print("Please insert your full puzzle input:")
 
 # Vars
+list = []
 
 # Main
 
 # Part 1
+list = re.findall("regexmonster", )
 
 # Part 2
