1861e275f285cd4c7a2b5c7ae3296bc1a91a4a34
Pavel Loubek <pav.loubek@gmail.com>
2025-02-07 12:58:20 +0100

    AoC3 part 1 done

--- AoC3.py
+++ AoC3.py
@@ -1,15 +1,25 @@
-# Modules - oh no, regex time :<
+# Modules
 import re
 
-# Info
-print("Please insert your full puzzle input:")
-
 # Vars
-list = []
+listMulsPart1 = []  # extracted muls from input
+listResultsPart1 = []   # multiplied all muls
+finalResultPart1 = ""   # all muls added up
+Part1RegexFind = r'mul\((\d{1,3}),(\d{1,3})\)'
 
-# Main
+# Info + Input
+inputLocation = input("Please insert your txt input location: ")
+    # C:\Users\loubek\Desktop\input.txt
+file = open(inputLocation, "r")
+fileString = file.read()
 
 # Part 1
-list = re.findall("regexmonster", )
+listMulsPart1 = re.findall(Part1RegexFind, fileString)
+for i in listMulsPart1:
+    listResultsPart1.append(int(i[0])*int(i[1]))
+finalResultPart1 = sum(listResultsPart1)
 
 # Part 2
+
+# Finish
+print("Result for part 1 is " + str(finalResultPart1))
