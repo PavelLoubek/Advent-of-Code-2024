c3b5afe7b3c1552bfa8d8a5357b84f949ef6e787
Pavel Loubek <pav.loubek@gmail.com>
2025-02-07 13:45:23 +0100

    AoC3 part 2 done

--- AoC3.py
+++ AoC3.py
@@ -7,6 +7,15 @@ listResultsPart1 = []   # multiplied all muls
 finalResultPart1 = ""   # all muls added up
 Part1RegexFind = r'mul\((\d{1,3}),(\d{1,3})\)'
 
+Part2RegexFindAll = r'mul\(\d{1,3},\d{1,3}\)|do\(\)|don\'t\(\)'
+Part2RegexFindMul = r'mul\((\d{1,3}),(\d{1,3})\)'
+mulEnabled = True
+listInputsPart2 = []  # extracted muls, do's and don'ts from input
+listResultsPart2 = []   # multiplied all filtered muls
+finalResultPart2 = ""   # all filtered muls added up
+
+
+
 # Info + Input
 inputLocation = input("Please insert your txt input location: ")
     # C:\Users\loubek\Desktop\input.txt
@@ -20,6 +29,18 @@ for i in listMulsPart1:
 finalResultPart1 = sum(listResultsPart1)
 
 # Part 2
+listInputsPart2 = re.findall(Part2RegexFindAll, fileString)
+for i in listInputsPart2:
+    if i == "do()":
+        mulEnabled = True
+    elif i == "don't()":
+        mulEnabled = False
+    else:
+        if mulEnabled:
+            mul = re.match(Part2RegexFindMul,i)
+            listResultsPart2.append(int(mul.group(1))*int(mul.group(2)))
+finalResultPart2 = sum(listResultsPart2)
 
 # Finish
 print("Result for part 1 is " + str(finalResultPart1))
+print("Result for part 2 is " + str(finalResultPart2))
