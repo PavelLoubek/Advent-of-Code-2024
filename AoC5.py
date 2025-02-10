cad289b7109b5722538cc665677f28c0cd181ba8
Pavel Loubek <pav.loubek@gmail.com>
2025-02-10 14:49:58 +0100

    AoC5 part 2 done

--- AoC5.py
+++ AoC5.py
@@ -11,6 +11,8 @@ listUpdates = fileSplitInputs[fileSplitInputs.index('')+1:]
 resultPart1 = 0
 resultPart2 = 0
 ruleValid = True
+listInvalidUpdates = []
+ruleApplied = True
 
 # Part 1
 for upd in listUpdates:
@@ -25,9 +27,27 @@ for upd in listUpdates:
     if ruleValid:
         resultPart1 += int(updList[len(updList)//2])
 
-
-
 # Part 2
+    else:
+        listInvalidUpdates.append(upd)
+
+for upd in listInvalidUpdates:
+    updList = upd.split(",")
+    while ruleApplied:
+        ruleApplied = False
+        for index in range(len(updList)):
+            for rule in listRules:
+                ruleList = rule.split("|")
+                if ruleList[0] == updList[index]:
+                    if ruleList[1] in updList[:index]:
+                        foundUpdValueIndex = updList.index(ruleList[1])
+                        updList.pop(foundUpdValueIndex)
+                        updList.insert(index,ruleList[1])
+                        ruleApplied = True
+    resultPart2 += int(updList[len(updList)//2])
+    ruleApplied = True
+
+
 
 # Finish
 print("Result for part 1 is " + str(resultPart1))
